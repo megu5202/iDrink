c2397315db1d998bbe33b4d6e9d371f5807e6485
jach9009 <jake.charland@gmail.com>
2014-04-27 21:20:18 -0600

    bac calculation done

--- iDrink/Statistics.m
+++ iDrink/Statistics.m
@@ -15,25 +15,24 @@
 }
 
 -(void)calculateBAC{
-    /*NSLog(@"updateBAC");
-    int bac = 0;
-    double genderConstant = -1;
-    NSString *gender = person.getGender;
-    NSNumber *weight = person.getWeight;
+    NSLog(@"updateBAC");
+    double bac = 0;
+    NSInteger gender = person.getGender;
+    double weight = person.getWeight;
     Session *session = person.getSession;
-    NSNumber *drinksDrank = session.getNumDrinks;
-    int bodyWaterConstant = 0;
-    int drinkingPeriod = 0;
+    double drinksDrank = session.getNumDrinks;
+    double bodyWaterConstant = 0;
+    double drinkingPeriod = 1;
+    double weightInKilos = weight/2.20462;
     
-    
-    if([gender  isEqual: @"male"]){
-        bodyWaterConstant = 0.58;
-    }
-    else{
+    if(gender == 1){
         bodyWaterConstant = 0.49;
     }
-    
-    bac = (.806 * drinksDrank * 1.2)/(bodyWaterConstant * )*/
+    else{
+        bodyWaterConstant = 0.58;
+    }
+    bac = ((.806 * drinksDrank * 1.2)/(bodyWaterConstant * weightInKilos)) - (.017 * drinkingPeriod);
+    [self setBACNumber:bac];
 }
 -(void)evaluateBAC{
     
